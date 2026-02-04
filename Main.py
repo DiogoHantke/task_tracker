2a5a5037da4788d7f8bd6dc3c4459906a4427be3
Diogo Hantke <hrdiogo2812@gmail.com>
2026-02-04 18:38:56 -0300

    foi feito o escopo de função de adicionar tarefas e resolvi alguns bugs lógicos

--- Main.py
+++ Main.py
@@ -1,66 +1,93 @@
-import json, time, sys, os
-from tabulate import tabulate
-
+import json, sys, os
+from tabulate import tabulate 
+#---------------------------------------------------------------------------------------
 def read_json():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, "tasks.json")
-
     try:
-        with open(file_path, "r", encoding="utf-8") as file:
-            tasks = json.load(file)
-            return tasks
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                tasks = json.load(file)
+                return tasks
 
-    except FileNotFoundError:
-        with open(file_path, "w", encoding="utf-8") as file:
-            tasks = {
-                []
-            }
-            json.dump(tasks, file, ensure_ascii=False, indent=2) #estudar mais
-            print("Arquivo de tarefas criado, adicione sua tarefa")
-            return None
+        except FileNotFoundError:
+            with open(file_path, "w", encoding="utf-8") as file:
+                tasks = {"tasks":[]}
+                json.dump(tasks, file, ensure_ascii=False, indent=2) #estudar mais
+                print("Arquivo de tarefas criado, adicione sua tarefa")
+                return tasks
 
+    except json.decoder.JSONDecodeError:
+        print("arquivo corrompido. ")
+        return None
+#---------------------------------------------------------------------------------------
 
 def write_json(tasks : dict):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, "tasks.json")
 
-    with open(file_path, "w", encoding="utf-8") as file:
-        json.dump(tasks, file, ensure_ascii=False, indent=2)
+    try:
+        with open(file_path, "w", encoding="utf-8") as file:
+            json.dump(tasks, file, ensure_ascii=False, indent=2)
 
-
+    except json.decoder.JSONDecodeError:
+        print("arquivo corrompido.")
+#---------------------------------------------------------------------------------------
 def task_add():
-    ...
+    tasks, new_id = read_json(), 0
 
+    if tasks is None: #melhorar tratamento de erro no arquivo
+        return None
+
+    list_id = [task['id'] for task in tasks['tasks']]
+
+    if tasks['tasks'] == []:
+        new_id = 0
+    else:
+        new_id = max(list_id)+1
+
+    #adicionar algoritmo de quebra de linha
+    
+    tasks['tasks'].append({
+        'id' : new_id,
+        'title' : input("Digite o título: "),
+        'description' : input("Digite a descrição: "),
+        'create_date' : os.popen('date /t').read().strip(),
+        'priority' : input("Digite a prioridade (Baixa, Média, Alta): "),
+        'status' : 'Pending'
+    })
+    
+    write_json(tasks)
+#---------------------------------------------------------------------------------------
+    
 def task_update():
     ...
 
 def task_drop():
     ...
-
+#---------------------------------------------------------------------------------------
 def task_list():
     tasks = read_json()
 
-    if tasks is None:
-        print("Não há nenhuma tarefa na lista")
-    else:
+    if tasks is not None:
         print(display_task(tasks))
-
+#---------------------------------------------------------------------------------------
 def display_task(tasks : dict):
 
-    tasks, data = [data for data in tasks['tasks']], []
+    tasks_list, data = [data for data in tasks['tasks']], []
 
-    for index in range(len(tasks)):
-        data.append(tasks[index].values())
+    for index in range(len(tasks_list)):
+        data.append(tasks_list[index].values())
     
     headers = ["id", "title", "description", "create_date", "priority", "status"]
     table = tabulate(data, headers=headers, tablefmt="fancy_grid")
 
     return table
-
+#---------------------------------------------------------------------------------------
 if __name__ == "__main__":
     os.system('cls')
-    argv = sys.argv
 
+    argv = sys.argv
     commands = {
         "task-add"    : task_add,
         "task-update" : task_update,
@@ -72,5 +99,5 @@ if __name__ == "__main__":
     try:
         commands[argv[1].lower()]() if argv[1].lower() in commands else print("ErrorCommand: Not found")
     except IndexError:
-        print("No command provided. Use one of: task-add, task-list, ...")
+        print("O comando não existe, tente task-list, task-add, task-update")
     

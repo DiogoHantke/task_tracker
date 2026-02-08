d16c1acb1ceced8d6330545792f8c11a7550b8c6
Diogo Hantke <hrdiogo2812@gmail.com>
2026-02-07 21:47:33 -0300

    função de identação do texto das tasks e função de update feitas

--- Main.py
+++ Main.py
@@ -18,6 +18,8 @@ def read_json():
         
     except json.JSONDecodeError:
         print("arquivo corrompido. ")
+        with open(file_path, "w", encoding="utf-8") as file:
+            json.dump({"tasks": []}, file, ensure_ascii=False, indent=2)
         return None
 
 def write_json(tasks : dict):
@@ -27,7 +29,7 @@ def write_json(tasks : dict):
     with open(file_path, "w", encoding="utf-8") as file:
         json.dump(tasks, file, ensure_ascii=False, indent=2)
 
-def task_add(argv):
+def task_add(argv : list):
     tasks, new_id = read_json(), 1
 
     argv_sep = argv[2:]
@@ -45,13 +47,12 @@ def task_add(argv):
     description = argv_sep[1].strip()
     priority = argv_sep[2].strip()
 
-    description_treated(description)
-
     tasks['tasks'].append({
         'id' : new_id,
-        'title' : title,
-        'description' : description,
+        'title' : description_treated(title),
+        'description' : description_treated(description),
         'create_date' : os.popen('date /t').read().strip(),# não sei como isso funciona
+        'update_date' : os.popen('date /t').read().strip(),# não sei como isso funciona
         'priority' : priority,
         'status' : 'Pending'
     })
@@ -59,14 +60,29 @@ def task_add(argv):
     write_json(tasks)
     task_list()
 
-def task_update():
-    tasks = read_json()
+def task_update(argv : list):
+    tasks, args = read_json(), argv[2:]
 
     if tasks is None:
         return 
+    
+    for index, id_task in enumerate(tasks['tasks']):
+
+        if int(args[0]) == int(id_task['id']):
+            tasks['tasks'][index]['priority'] = args[1]
+            tasks['tasks'][index]['update_date'] = os.popen('date /t').read().strip()
+            break
+        
+        elif index >= len(tasks['tasks'])-1:
+            print("Id não existe.")
+            return 
+        
+    write_json(tasks)
+    task_list()
+    return
 
 def task_drop(argv : int): #colocar um tratamento se caso nao existir o id que o user colocou
-    tasks = read_json()
+    tasks, argv = read_json(), int(argv[2])
 
     if tasks is None:
         return 
@@ -98,7 +114,7 @@ def display_task(tasks : dict):
     for index in range(len(tasks_list)):
         data.append(tasks_list[index].values())
     
-    headers = ["id", "title", "description", "create_date", "priority", "status"]
+    headers = ["id", "title", "description", "create_date", "update_date", "priority", "status"]
     table = tabulate(data, headers=headers, tablefmt="fancy_grid")
 
     return table
@@ -111,7 +127,7 @@ Uso:
 
 Comandos disponíveis:
 
-    task-add
+    task-add "title" "description" "priority"
         Adiciona uma nova tarefa.
         Solicita título, descrição e prioridade.
 
@@ -143,7 +159,18 @@ Exemplos:
 """)
     
 def description_treated(description : str):
-    ...
+    description_treated, num, number = [], 0, 50
+
+    for string in description.split():
+        num+=len(string)+1
+        if num >= number:
+            num = len(string)+1
+            description_treated.append(f"\n{string}")
+        else:
+            description_treated.append(string)
+
+    return ' '.join(description_treated)
+
 
 if __name__ == "__main__":
     os.system('cls')
@@ -152,20 +179,16 @@ if __name__ == "__main__":
 
     commands = {
         "task-add"    : lambda: task_add(argv),
-        "task-update" : lambda: task_update(),
-        "task-drop"   : lambda: task_drop(int(argv[2])),
+        "task-update" : lambda: task_update(argv),
+        "task-drop"   : lambda: task_drop(argv),
         "task-list"   : lambda: task_list(),
         "task-help"   : lambda: task_help(),
         "task-exit"   : lambda: exit()
     }
 
-    frase = "Aprender python do básico ao avançado para conseguir\nfazer aplicações profissionais de qualquer coisa mais algo"
+    try:
+        commands[argv[1].lower()]() if argv[1].lower() in commands else task_help()
 
-    print(frase)
-
-    # try:
-    #     commands[argv[1].lower()]() if argv[1].lower() in commands else task_help()
-
-    # except IndexError:
-    #     print("ERROR: command not found\n")
-    #     task_help() 
+    except IndexError:
+        print("ERROR: command not found\n")
+        task_help() 
